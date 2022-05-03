6e413224b73bdb2e74a182ae29a9220ac227dce9
lubaroli <lucas.barcelos@gmail.com>
2022-05-03 16:27:18 +1000

    SVGD can receive external kernel evaluations.

--- stein_mpc/inference/svgd.py
+++ stein_mpc/inference/svgd.py
@@ -1,9 +1,8 @@
-from typing import Callable
+from typing import Callable, Tuple
 
 import torch
 import torch.autograd as autograd
 import torch.optim as optim
-from torch.optim import optimizer
 from tqdm import trange
 
 from ..kernels import BaseKernel
@@ -30,7 +29,9 @@ class SVGD:
         self.optimizer_class = optimizer_class
         self.opt_args = opt_args
 
-    def _velocity(self, X: torch.Tensor, grad_log_p: torch.Tensor) -> torch.Tensor:
+    def _velocity(
+        self, X: torch.Tensor, grad_log_p: torch.Tensor, **kwargs
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
         if self.log_p is None and grad_log_p is None:
             raise ValueError(
                 "SVGD needs a function to evaluate the log probability of the target",
@@ -56,20 +57,22 @@ class SVGD:
             score_func = score_func + log_prior_grad
             X.detach_()
 
-        if hasattr(self.kernel, "analytic_grad") and self.kernel.analytic_grad:
-            # grad_k is batch x batch x dim
-            k_xx, grad_k = self.kernel(X, X)
-            grad_k = grad_k.sum(1)  # aggregates gradient wrt to first input
+        if not ("k_xx" in kwargs and "grad_k" in kwargs):
+            if hasattr(self.kernel, "analytic_grad") and self.kernel.analytic_grad:
+                # grad_k is batch x batch x dim
+                k_xx, grad_k = self.kernel(X, X)
+                grad_k = grad_k.sum(1)  # aggregates gradient wrt to first input
+            else:
+                X = X.detach().requires_grad_(True)
+                k_xx = self.kernel(X, X.detach(), compute_grad=False)
+                grad_k = autograd.grad(-k_xx.sum(), X)[0]
+                X.detach_()
+                k_xx.detach_()
         else:
-            X = X.detach().requires_grad_(True)
-            k_xx = self.kernel(X, X.detach(), compute_grad=False)
-            grad_k = autograd.grad(-k_xx.sum(), X)[0]
-            X.detach_()
-            k_xx.detach_()
+            k_xx = kwargs["k_xx"]
+            grad_k = kwargs["grad_k"]
 
         velocity = (k_xx @ score_func + grad_k) / shape[0]
-        # print(f"Grad L norm: {score_func.norm():.2f}")
-        # print(f"Grad K norm: {grad_k.norm():.2f}")
         return -velocity.reshape(shape), loss
 
     def step(
@@ -77,17 +80,18 @@ class SVGD:
         X: torch.Tensor,
         grad_log_p: torch.Tensor = None,
         optimizer: optim.Optimizer = None,
+        **kwargs,
     ):
         # if no optimizer given, redefine optimizer to include x as parameter, since x
         # could change between calls, n.b. this will reset momentum based optimizers
         if optimizer is None:
             # optimizer = self.optimizer_class(params=[X], **self.opt_args)
-            grad, loss = self._velocity(X, grad_log_p)
+            grad, loss = self._velocity(X, grad_log_p, **kwargs)
             X = X + self.opt_args["lr"] * grad
 
         def closure():
             optimizer.zero_grad()
-            X.grad, loss = self._velocity(X, grad_log_p)
+            X.grad, loss = self._velocity(X, grad_log_p, **kwargs)
             return loss
 
         if isinstance(optimizer, LBFGS):
@@ -104,32 +108,34 @@ class SVGD:
         opt_state: dict = None,
         n_steps: int = 100,
         debug: bool = False,
-    ) -> list:
+        **kwargs,
+    ) -> tuple:
         X = particles.detach()
         # defined here to include x as parameter, since x could change between calls
         optimizer = self.optimizer_class(params=[X], **self.opt_args)
         if opt_state is not None:
             optimizer.load_state_dict(opt_state)
         grad_log_p = None
-        data = []
+        data_dict = {}
         if debug:
             iterator = trange(n_steps, position=0, leave=True)
         else:
             iterator = range(n_steps)
 
         X_seq = X.clone().unsqueeze(0)
-        for _ in iterator:
+        for i in iterator:
             if score_estimator is not None:
                 X.requires_grad_(True)
                 grad_log_p, datum = score_estimator(X)
-                data.append(datum)
+                data_dict[i] = {**datum}
+                kwargs.update(datum)
                 X.detach_()
-            self.step(X, grad_log_p, optimizer)
+            self.step(X, grad_log_p, optimizer, **kwargs)
             X_seq = torch.cat([X_seq, X.unsqueeze(0)], dim=0)
             if debug:
                 iterator.set_postfix(loss=X.grad.detach().norm(), refresh=False)
-        X.detach_()
-        return X_seq, data, optimizer.state_dict()
+        data_dict["trace"] = X_seq
+        return data_dict, optimizer.state_dict()
 
 
 class ScaledSVGD(SVGD):
@@ -164,7 +170,7 @@ class ScaledSVGD(SVGD):
             )
         # batch should be X.shape[0], remaining dims will be flattened
         shape = X.shape
-        X = X.reshape(shape[0], -1)
+        X = X.flatten(start_dim=1)
         if grad_log_p is None:
             X = X.detach().requires_grad_(True)
             log_lik = self.log_p(X.reshape(shape)).sum()
@@ -173,14 +179,7 @@ class ScaledSVGD(SVGD):
             loss = -log_lik.detach()
         else:
             score_func = grad_log_p  # expects the gradient to *minimize* the loss
-            loss = score_func.norm()
-
-        if self.log_prior is not None:
-            X = X.detach().requires_grad_(True)
-            log_prior_sum = self.log_prior(X.reshape(shape)).sum()
-            log_prior_grad = torch.autograd.grad(log_prior_sum, X)[0].detach()
-            score_func = score_func + log_prior_grad
-            X.detach_()
+            loss = score_func.norm()  # this should be the NLL
 
         if self.metric.lower() == "gaussnewton":
             # M = self._estimate_gn_hessian(score_func).mean(dim=0)
@@ -210,12 +209,16 @@ class ScaledSVGD(SVGD):
             X.detach_()
             k_xx.detach_()
 
+        if self.log_prior is not None:
+            X = X.detach().requires_grad_(True)
+            log_prior_sum = self.log_prior(X.reshape(shape)).sum()
+            log_prior_grad = torch.autograd.grad(log_prior_sum, X)[0].detach()
+            score_func = score_func + log_prior_grad
+            X.detach_()
+
         velocity = (k_xx @ score_func + grad_k) / shape[0]
         if self.precondition:
             velocity = torch.linalg.solve(M, velocity.T).T
-        # print(f"Grad L norm: {score_func.norm():.2f}")
-        # print(f"Grad K norm: {grad_k.norm():.2f}")
-        # print(f"Velocity norm: {velocity.norm():.2f}")
         return -velocity.reshape(shape), loss
 
     def _psd_estimate_gn_hessian(self, jacobian, eps=1e-3):
