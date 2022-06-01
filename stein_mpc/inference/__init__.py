45f34cb0bd3757550a6ec83e1824d62c4640ab2c
lubaroli <lucas.barcelos@gmail.com>
2022-06-01 16:01:10 +1000

    Created TrajectorySVGD with action primitives

--- stein_mpc/inference/__init__.py
+++ stein_mpc/inference/__init__.py
@@ -1,11 +1,14 @@
 from .svgd import SVGD, ScaledSVGD
-from .likelihoods import GaussianLikelihood, ExponentiatedUtility
+from .trajectory_svgd import TrajectorySVGD
+from .likelihoods import GaussianLikelihood, CostLikelihood, ExponentiatedUtility
 from .mpf import MPF
 
 __all__ = [
     "SVGD",
     "ScaledSVGD",
+    "TrajectorySVGD",
     "GaussianLikelihood",
+    "CostLikelihood",
     "ExponentiatedUtility",
     "MPF",
 ]
